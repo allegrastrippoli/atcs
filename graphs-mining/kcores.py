04bcb1aec2c2b4ab90cf4a05396335a0c3b2736f
Allegra Strippoli <allegra@strippoli.eu>
2024-06-03 15:33:04 +0200

    fix: deg initialization

--- graphs-mining/kcores.py
+++ graphs-mining/kcores.py
@@ -1,22 +1,23 @@
 import utility as u
+import matplotlib.pyplot as plt
 import networkx as nx
 
 #        COUNTING SORT
 #         0  1  2  3  4  5
-# deg:   [1, 4, 1, 1, 2, 1] => the degree of node 0 is 1
-# bins:  [0, 4, 1, 0, 1]    => 4 nodes have degree equal to 1
-# bins:  [0, 0, 4, 5, 5]    => nodes with degree equal to 2, start at pos 4
+# deg:   [] => the degree of node 0 is 1
+# bins:  []    => 4 nodes have degree equal to 1
+# bins:  []    => nodes with degree equal to 2, start at pos 4
 # sort: [0, 2, 3, 5, 4, 1]  => node with id 3 has degree 1 and has position 2
-# pos:  [0, 5, 1, 2, 4, 3] => sort: pos=5, id=1, pos: pos=1, id=5
+# pos:  [0, 5, 1, 2, 4, 3] => sort: pos=2, id=3, pos: pos=3, id=2
 
 def get_degree(graph):
-    deg = []
-    for node in graph:
-        deg.append(graph.degree(node))
+    deg = [0] * len(graph.nodes())
+    for node in graph.nodes():
+        deg[node] = graph.degree(node)
     return deg
 
 def counting_sort(graph, deg):
-    bins = [0] * (graph.number_of_nodes())
+    bins = [0] * (max(deg)+1)
     for elem in deg:
         bins[elem] += 1
 
@@ -40,19 +41,22 @@ def counting_sort(graph, deg):
 
 # sort: [3, ., 0, ., ., .]
 # pos:  [2, ., ., 0, ., .] node with id 3 has position 0, node with id 0 has position 2 
-
 def decrease_degree(node, deg, bins, sort, pos):
-    # node = 3
-    posNode = pos[node] # 2
-    posFirstNodeWithSameDegree = bins[deg[node]] # 0
-    nodef = sort[posFirstNodeWithSameDegree] # 0
+    posNode = pos[node] 
+    posFirstNodeWithSameDegree = bins[deg[node]] 
+    if posFirstNodeWithSameDegree != node:
+        nodef = sort[posFirstNodeWithSameDegree] 
 
-    sort[posNode] = nodef 
-    sort[posFirstNodeWithSameDegree] = node
-    pos[node] = posFirstNodeWithSameDegree
-    pos[nodef] = posNode
+        sort[posNode] = nodef 
+        sort[posFirstNodeWithSameDegree] = node
+        pos[node] = posFirstNodeWithSameDegree
+        pos[nodef] = posNode
 
-    bins[deg[node]] += 1
+    if bins[deg[node]]+1 >= len(sort):
+        bins = bins[:len(bins)-1]
+    else:
+        bins[deg[node]] += 1 
+
     deg[node] -= 1
 
     return deg, bins, sort, pos
@@ -75,21 +79,27 @@ def find_coreness(G):
         for neighbour in G.neighbors(node):
             if deg[neighbour] > deg[node]:
                 deg, bins, sort, pos = decrease_degree(neighbour, deg, bins, sort, pos)
-    
-    max_value = max(deg)
 
+    
+    # HELLO = nx.k_core(G)
+    # nx.draw_networkx(HELLO, with_labels=True)
+    # plt.show()
+    max_value = max(deg)
     V1 = set()
     for i in range(0, len(deg)):
         if deg[i] == max_value:
             V1.add(i)
-        
+
     return V1
 
 
 def main():
+    # G = u.test_graph()
     G = u.parser('moreno_lesmis/out.moreno_lesmis_lesmis')
     V1 = find_coreness(G)
-    u.draw_graph(G, V1, 'kcore2')
+    u.draw_graph(G, V1, 'kcore')
+
+
 
 if __name__ == "__main__":
     main()
